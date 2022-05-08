513428598a26659ab7d2a8cfecb595d866d20404
Leonardo Dos Santos Lopes <leos_lopes@hotmail.com>
2022-05-08 05:36:41 -0300

    add port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,11 @@ import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
-const port = 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-	console.log("Server started on port: " + port);
+app.listen(process.env.PORT || 3333, () => {
+	console.log("Server started on port: " + process.env.PORT || 3333);
 });
